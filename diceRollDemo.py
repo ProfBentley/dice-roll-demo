b41e88c4be21244f2878f8b0ff96b9460187b59c
ProfBentley <bentley_jandlyn@wheatoncollege.edu>
2022-11-28 16:36:18 -0500

    First commit

---
+++ diceRollDemo.py
@@ -0,0 +1,19 @@
+import random
+
+
+# Make a dice throw game
+
+def main():
+    # get input from a keyboard: input("Some kind of prompt: ")
+    name = input("What is your name?: ")
+    print(f"Hello, {name}!")
+    
+    # make two individual die rolls
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    
+    print(f"You rolled a {dice1} and a {dice2}!")
+
+
+main()
+
